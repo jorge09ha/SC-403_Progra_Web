c7d978057f568d597b80577e51de1308c2bbf75f
Jorge Hernandez <jorge09ha@gmail.com>
2023-03-24 01:40:49 -0600

    Correcciones del trabajo en clase

--- Tienda/src/main/java/com/Tienda/controller/PersonaController.java
+++ Tienda/src/main/java/com/Tienda/controller/PersonaController.java
@@ -36,6 +36,14 @@ public class PersonaController {
         return "personas";
     }
 
+    @GetMapping("/pais")
+    public String indexP(Model model) {
+        List<Pais> lista = paisService.listCountry();
+        model.addAttribute("titulo", "Tabla Paises");
+        model.addAttribute("paises", lista);
+        return "paises";
+    }
+
     @GetMapping("/personaN")
     public String crearPersona(Model model) {
         List<Pais> listaPaises = paisService.listCountry();
@@ -51,6 +59,12 @@ public class PersonaController {
         return "redirect:/persona";
     }
 
+    @GetMapping("/delete/{id}")
+    public String eliminarDistrito(@PathVariable Long id) {
+        personaService.delete(id);
+        return "redirect:/persona";
+    }
+
     @GetMapping("/editPersona/{id}")
     public String editarPersona(@PathVariable("id") Long idPersona, Model model) {
         Persona persona = personaService.getPersonaById(idPersona);
@@ -58,13 +72,27 @@ public class PersonaController {
         model.addAttribute("titulo", "Editar Persona");
         model.addAttribute("persona", persona);
         model.addAttribute("paises", listaPaises);
-        return "crear";
+        return "editar";
+    }
+
+    @PostMapping("/editPersona/{id}")
+    public String actualizarPersona(@PathVariable Long id, @ModelAttribute("Persona") Persona a) {
+        Persona editar = personaService.getPersonaById(id);
+        editar.setId(id);
+        editar.setNombre(a.getNombre());
+        editar.setApellido1(a.getApellido1());
+        editar.setApellido2(a.getApellido2());
+        editar.setEmail(a.getEmail());
+        editar.setTelefono(a.getTelefono());
+        editar.setPais(a.getPais());
+        personaService.savePersona(editar);
+        return "redirect:/persona";
     }
 
     @GetMapping("/search")
     public ModelAndView searchByName(@RequestParam("apellido1") String apellido1) {
         List<Persona> apellido = personaService.findByApellido1(apellido1);
-        ModelAndView modelAndView = new ModelAndView("personas1");
+        ModelAndView modelAndView = new ModelAndView("personasB");
         modelAndView.addObject("apellido", apellido);
         return modelAndView;
     }
